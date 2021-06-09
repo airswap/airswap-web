25abcf140634d5a0dc63a71d1a95978d2746c7c7
Greypixel <greypixel@gpxl.tech>
2021-06-09 10:27:47 +0100

    Don't cache translations in localStorage

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,14 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import Backend from "i18next-chained-backend";
-import LocalStorageBackend from "i18next-localstorage-backend"; // primary use cache
-import HttpApi from "i18next-http-backend";
+import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   // load translation using http -> see /public/locales
-  .use(Backend)
+  .use(HttpBackend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
@@ -28,27 +26,7 @@ i18n
       useSuspense: true,
     },
     backend: {
-      backends: [
-        LocalStorageBackend, // primary
-        HttpApi, // secondary
-      ],
-      backendOptions: [
-        // Local storage options
-        {
-          prefix: "airswap/i18n/",
-          // 4 weeks expiry
-          expirationTime: 4 * 7 * 24 * 60 * 60 * 1000,
-          // Version applied to all languages, can be overriden using versions
-          defaultVersion: "",
-          // lang specific versions
-          versions: {},
-          store: window.localStorage,
-        },
-        // HTTP options
-        {
-          loadPath: "/locales/{{lng}}/{{ns}}.json",
-        },
-      ],
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
     },
   });
 
