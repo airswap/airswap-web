6f96a121317bbb25c1f625c97a7a6c1b2db65c9e
Don Mosites <don.mosites@fluidity.io>
2021-06-09 12:30:25 +0200

    Adding lint command

--- src/features/wallet/Wallet.tsx
+++ src/features/wallet/Wallet.tsx
@@ -11,7 +11,6 @@ import {
   incrementBalanceBy,
   requestActiveTokenAllowances,
   requestActiveTokenBalances,
-  selectAllowances,
   selectBalances,
   setAllowance,
 } from "../balances/balancesSlice";
