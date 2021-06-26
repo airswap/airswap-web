629ad0669036c90519f2f6fbe43d0bdb0c1ca1b8
hmalik88 <hbmalik88@gmail.com>
2021-06-26 06:31:42 -0400

    added SubmittedApproval type to submitTransactions action creator to accomodate for approval transactions

--- src/features/transactions/transactionActions.ts
+++ src/features/transactions/transactionActions.ts
@@ -1,7 +1,7 @@
 import { createAction } from "@reduxjs/toolkit";
-import { SubmittedTransaction } from './transactionsSlice'
+import { SubmittedTransaction, SubmittedApproval } from './transactionsSlice'
 
-const submitTransaction = createAction<SubmittedTransaction>("transaction/submitTransaction");
+const submitTransaction = createAction<SubmittedTransaction | SubmittedApproval>("transaction/submitTransaction");
 
 const declineTransaction = createAction<{
   hash: string;
