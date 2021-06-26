e8e5c2bbbf0305730940bdcbc73a0518013b4b96
Cody Enokida <codyenokida@gmail.com>
2021-06-26 03:33:46 -0700

    Fixed bug; transactions accidentally persisted in localStorage through chain id change

--- src/features/metadata/metadataSubscriber.ts
+++ src/features/metadata/metadataSubscriber.ts
@@ -1,37 +1,70 @@
 import { store } from "../../app/store";
 import {
   getActiveTokensLocalStorageKey,
-  getTransactionsLocalStorageKey } from "./metadataApi";
-import { SubmittedTransaction } from '../transactions/transactionsSlice';
-
+  getTransactionsLocalStorageKey,
+} from "./metadataApi";
+import {
+  SubmittedTransaction,
+  TransactionsState,
+} from "../transactions/transactionsSlice";
 
 export const subscribeToSavedTokenChangesForLocalStoragePersisting = () => {
   const cache: {
     [chainId: number]: string[];
   } = {};
   const transactionCache: {
-    [chainId: number]: SubmittedTransaction[];
-  } = {}
+    [chainId: number]: {
+      [address: string]: SubmittedTransaction[];
+    };
+  } = {};
+
+  let currentTransaction: TransactionsState;
+
   store.subscribe(() => {
     const { wallet, metadata, transactions } = store.getState();
     if (!wallet.connected) return;
-    // persist all transactions to localStorage
-    const transactionCached = transactionCache[wallet.chainId!];
-    if (transactions.all.length && transactionCached !== transactions.all) {
-      localStorage.setItem(
-        getTransactionsLocalStorageKey(wallet.chainId!, wallet.address!),
-        JSON.stringify(transactions)
-      );
+
+    let previousTransaction = currentTransaction;
+    currentTransaction = transactions;
+
+    if (wallet && transactions) {
+      if (previousTransaction !== currentTransaction) {
+        // handles change in transactions and persists all transactions to localStorage
+        // TODO: don't save all transactions (e.g. max 10 transactions)
+        const txs: TransactionsState = JSON.parse(
+          localStorage.getItem(
+            getTransactionsLocalStorageKey(wallet.chainId!, wallet.address!)
+          )!
+        ) || { all: [] };
+
+        if (transactionCache[wallet.chainId!] === undefined) {
+          transactionCache[wallet.chainId!] = {};
+          transactionCache[wallet.chainId!][wallet.address!] = txs.all;
+        }
+        if (
+          transactions.all.length &&
+          transactionCache[wallet.chainId!][wallet.address!] !==
+            transactions.all
+        ) {
+          transactionCache[wallet.chainId!][wallet.address!] = transactions.all;
+          localStorage.setItem(
+            getTransactionsLocalStorageKey(wallet.chainId!, wallet.address!),
+            JSON.stringify(transactions)
+          );
+        }
+      }
     }
 
-    // active tokens have changed, persist to local storage.
-    const cached = cache[wallet.chainId!];
-    if (metadata.tokens.active.length && cached !== metadata.tokens.active) {
-      cache[wallet.chainId!] = metadata.tokens.active;
-      localStorage.setItem(
-        getActiveTokensLocalStorageKey(wallet.chainId!),
-        metadata.tokens.active.join(",")
-      );
+    if (wallet && metadata) {
+      // active tokens have changed, persist to local storage.
+      const cached = cache[wallet.chainId!];
+      if (metadata.tokens.active.length && cached !== metadata.tokens.active) {
+        cache[wallet.chainId!] = metadata.tokens.active;
+        localStorage.setItem(
+          getActiveTokensLocalStorageKey(wallet.chainId!),
+          metadata.tokens.active.join(",")
+        );
+      }
     }
   });
 };
