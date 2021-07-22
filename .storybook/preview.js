5100e1e4a45474e5b1643f0bbe59cea263f3059f
Greypixel <greypixel@gpxl.tech>
2021-07-22 12:40:24 +0100

    Add global style to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,26 +3,11 @@ import { ThemeProvider } from "styled-components/macro";
 import { addDecorator } from "@storybook/react";
 import { withThemes } from "@react-theming/storybook-addon";
 import { darkTheme, lightTheme } from "../src/style/themes";
+import GlobalStyle from "../src/style/GlobalStyle";
 import "../src/i18n/i18n";
 import "../src/index.css";
 
 const themingDecorator = withThemes(ThemeProvider, [darkTheme, lightTheme]);
-
-addDecorator(themingDecorator);
-
-export const onThemeSwitch = (context) => {
-  const { theme } = context;
-
-  const background = theme.name === "dark" ? "#060607" : "white";
-  const parameters = {
-    backgrounds: {
-      default: background,
-    },
-  };
-  return {
-    parameters,
-  };
-};
 
 // Suspense needed for i18n.
 export const decorators = [
@@ -33,4 +18,11 @@ export const decorators = [
       </Suspense>
     );
   },
+  (story) => (
+    <>
+      <GlobalStyle />
+      {story()}
+    </>
+  ),
+  themingDecorator,
 ];
