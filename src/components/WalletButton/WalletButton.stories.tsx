364a5b25b0414745fa8f543c4edfc8a54f0d597f
Cody Enokida <codyenokida@gmail.com>
2021-08-13 01:09:38 -0700

    lint changes

--- src/components/WalletButton/WalletButton.stories.tsx
+++ src/components/WalletButton/WalletButton.stories.tsx
@@ -1,6 +1,6 @@
 import { Story, Meta } from "@storybook/react";
-import { SubmittedTransaction } from "../../features/transactions/transactionsSlice";
 
+import { SubmittedTransaction } from "../../features/transactions/transactionsSlice";
 import { WalletButton, WalletButtonProps } from "./WalletButton";
 
 export default {
@@ -34,11 +34,11 @@ const transactions: SubmittedTransaction[] = [
       senderAmount: "10000000000000000",
       r: "0x43cff0758ed93222938094c2f016b3f73ca8ec7b6e73fbeb11fc04f88c452cde",
       s: "0x43fa4900adf33a6011c26d96186b02d5920afbf73f3a1bd807cde1113bea4d3a",
-      v: "27"
+      v: "27",
     },
     hash: "0xa1ea8106cca703f5286c3ca4c739f74463db991cfdaf96bba638b628bbad5f68",
     status: "succeeded",
-    timestamp: 1628840183271
+    timestamp: 1628840183271,
   } as SubmittedTransaction,
   {
     type: "Order",
@@ -54,11 +54,11 @@ const transactions: SubmittedTransaction[] = [
       senderAmount: "10000000000000000",
       r: "0xde1bb784ad81abf3f62f906543b5d8320172cffbb181e9336dba2640d9702bcb",
       s: "0x4266def0208ca3ae5bd3acb5b8ec949a9d0ad974ca76bb145d72ab69c72f8eac",
-      v: "28"
+      v: "28",
     },
     hash: "0x0268eddd12d466b19fb1ba05789e4fc8e6a4cfbdb2faf488acc93709dc567263",
     status: "succeeded",
-    timestamp: 1628840176458
+    timestamp: 1628840176458,
   } as SubmittedTransaction,
   {
     type: "Order",
@@ -74,68 +74,72 @@ const transactions: SubmittedTransaction[] = [
       senderAmount: "10000000000000000",
       r: "0x32fcfd8d0fdd8cbf1087f296511755fb6525ddd577b1178eebc69ea041c0721c",
       s: "0x65ae2d37ef4a8b3694c443d15579b0ee1720392581a3f181414c3289cb1fa7b8",
-      v: "27"
+      v: "27",
     },
     hash: "0xe594e34a2353164b9ecf1c91d00fcc6c07f8ae281e90d34b3a02f05321f79475",
     status: "succeeded",
-    timestamp: 1628840166870
-  } as SubmittedTransaction
+    timestamp: 1628840166870,
+  } as SubmittedTransaction,
 ];
 
 const tokens = [
   {
     chainId: 4,
-    address: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea',
-    name: 'Dai Stablecoin',
-    symbol: 'DAI',
+    address: "0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea",
+    name: "Dai Stablecoin",
+    symbol: "DAI",
     decimals: 18,
-    logoURI: 'https://raw.githubusercontent.com/compound-finance/token-list/master/assets/asset_DAI.svg'
+    logoURI:
+      "https://raw.githubusercontent.com/compound-finance/token-list/master/assets/asset_DAI.svg",
   },
   {
     chainId: 4,
-    address: '0xd9ba894e0097f8cc2bbc9d24d308b98e36dc6d02',
-    name: 'Tether USD',
-    symbol: 'USDT',
+    address: "0xd9ba894e0097f8cc2bbc9d24d308b98e36dc6d02",
+    name: "Tether USD",
+    symbol: "USDT",
     decimals: 6,
-    logoURI: 'https://raw.githubusercontent.com/compound-finance/token-list/master/assets/asset_USDT.svg'
+    logoURI:
+      "https://raw.githubusercontent.com/compound-finance/token-list/master/assets/asset_USDT.svg",
   },
   {
     chainId: 4,
-    address: '0xbf7a7169562078c96f0ec1a8afd6ae50f12e5a99',
-    name: 'Basic Attention Token',
-    symbol: 'BAT',
+    address: "0xbf7a7169562078c96f0ec1a8afd6ae50f12e5a99",
+    name: "Basic Attention Token",
+    symbol: "BAT",
     decimals: 18,
-    logoURI: 'https://raw.githubusercontent.com/compound-finance/token-list/master/assets/asset_BAT.svg'
+    logoURI:
+      "https://raw.githubusercontent.com/compound-finance/token-list/master/assets/asset_BAT.svg",
   },
   {
     chainId: 4,
-    address: '0xd6801a1dffcd0a410336ef88def4320d6df1883e',
-    name: 'Compound Ether',
-    symbol: 'cETH',
+    address: "0xd6801a1dffcd0a410336ef88def4320d6df1883e",
+    name: "Compound Ether",
+    symbol: "cETH",
     decimals: 8,
-    logoURI: 'https://raw.githubusercontent.com/compound-finance/token-list/master/assets/ctoken_eth.svg'
+    logoURI:
+      "https://raw.githubusercontent.com/compound-finance/token-list/master/assets/ctoken_eth.svg",
   },
   {
     chainId: 4,
-    address: '0x577d296678535e4903d59a4c929b718e1d575e0a',
-    name: 'Wrapped BTC',
-    symbol: 'WBTC',
-    decimals: 8
+    address: "0x577d296678535e4903d59a4c929b718e1d575e0a",
+    name: "Wrapped BTC",
+    symbol: "WBTC",
+    decimals: 8,
   },
   {
-    name: 'Wrapped Ether',
-    address: '0xc778417e063141139fce010982780140aa0cd5ab',
+    name: "Wrapped Ether",
+    address: "0xc778417e063141139fce010982780140aa0cd5ab",
     decimals: 18,
-    symbol: 'WETH',
-    chainId: 4
+    symbol: "WETH",
+    chainId: 4,
   },
   {
-    name: 'AirSwap Token',
-    address: '0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8',
+    name: "AirSwap Token",
+    address: "0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8",
     decimals: 4,
-    symbol: 'AST',
-    chainId: 4
-  }
+    symbol: "AST",
+    chainId: 4,
+  },
 ];
 
 export const NotConnected = Template.bind({});
@@ -173,4 +177,3 @@ NoTransactions.args = {
   chainId: 4,
   tokens: tokens,
 };
-
