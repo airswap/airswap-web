f01bfc484c5879f437382b6a993c7d4facb050b0
Piers <bonkbonksourman@gmail.com>
2021-08-21 11:34:12 +0200

    fix/remove-tailwind: Removed tailwind, added styles to GlobalStyle 2

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,9 @@
 import { Suspense } from "react";
 import { ThemeProvider } from "styled-components/macro";
-import { addDecorator } from "@storybook/react";
 import { withThemes } from "@react-theming/storybook-addon";
 import { darkTheme, lightTheme } from "../src/style/themes";
 import GlobalStyle from "../src/style/GlobalStyle";
 import "../src/i18n/i18n";
-import "../src/index.css";
 
 export const onThemeSwitch = (context) => {
   const { theme } = context;
