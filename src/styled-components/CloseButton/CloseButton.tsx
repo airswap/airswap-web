e38cf3d69248e0ff2888622d74f79f1cb5294f3c
Piers <bonkbonksourman@gmail.com>
2021-09-10 10:53:20 +0200

    Added Overlay component in storybook

--- src/styled-components/CloseButton/CloseButton.tsx
+++ src/styled-components/CloseButton/CloseButton.tsx
@@ -7,10 +7,11 @@ const CloseButton = styled(IconButton)`
   display: flex;
   justify-content: center;
   align-items: center;
+  border: 1px solid ${(props) => props.theme.colors.borderGrey};
   width: 1.5rem;
   height: 1.5rem;
   padding: 0;
-  border: 1px solid ${(props) => props.theme.colors.borderGrey};
+  background: ${(props) => props.theme.colors.black};
 
   &:hover {
     border-color: ${(props) => convertHexToRGBA(props.theme.colors.white, 0.5)};
