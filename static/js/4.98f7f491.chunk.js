89b2364ef1861230638d71f0b6cfb645d0b44135
hmalik88 <41640681+hmalik88@users.noreply.github.com>
2021-09-20 08:50:53 +0000

    Deploying to gh-pages from @ airswap/airswap-web@01c83254941e8bc53986549d14d7399f5819b661 🚀

---
+++ static/js/4.98f7f491.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpairswap-web"]=this["webpackJsonpairswap-web"]||[]).push([[4],{716:function(n,i){},769:function(n,i){},829:function(n,i){}}]);
+//# sourceMappingURL=4.98f7f491.chunk.js.map
--- static/js/4.b95fc521.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpairswap-web"]=this["webpackJsonpairswap-web"]||[]).push([[4],{752:function(n,i){},805:function(n,i){},865:function(n,i){}}]);
-//# sourceMappingURL=4.b95fc521.chunk.js.map
