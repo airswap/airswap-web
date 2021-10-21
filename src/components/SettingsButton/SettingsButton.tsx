700419081ec57a8c9073bfd08b786f81e9eab739
Cody Enokida <codyenokida@gmail.com>
2021-10-20 22:25:13 -0700

    Added escape key and off click handler to close popover

--- src/components/SettingsButton/SettingsButton.tsx
+++ src/components/SettingsButton/SettingsButton.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useRef } from "react";
+
 import SettingsPopover from "../SettingsPopover/SettingsPopover";
 import {
   SettingIconButtonContainer,
@@ -13,14 +15,45 @@ const SettingsButton = ({
   settingsOpen,
   setSettingsOpen,
 }: SettingsButtonType) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = useCallback(
+    (e) => {
+      if (containerRef.current && containerRef.current.contains(e.target)) {
+        return;
+      }
+      setSettingsOpen(false);
+    },
+    [setSettingsOpen]
+  );
+
+  const handleEscKey = useCallback(
+    (e) => {
+      if (e.keyCode === 27) {
+        setSettingsOpen(false);
+      }
+    },
+    [setSettingsOpen]
+  );
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick);
+    document.addEventListener("keydown", handleEscKey, false);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+      document.removeEventListener("keydown", handleEscKey, false);
+    };
+  }, [handleClick, handleEscKey]);
+
   return (
-    <SettingIconButtonContainer>
+    <SettingIconButtonContainer ref={containerRef}>
       <SettingsIconButton
         iconSize={1.5}
         icon="settings"
         onClick={() => setSettingsOpen(!settingsOpen)}
       />
-      {settingsOpen ? <SettingsPopover /> : null}
+      {settingsOpen && <SettingsPopover />}
     </SettingIconButtonContainer>
   );
 };
