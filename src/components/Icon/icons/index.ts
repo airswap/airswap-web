a6ebe598b0577261cd2a7443ef79d7e920a08454
Piers <bonkbonksourman@gmail.com>
2021-10-23 03:59:57 +0200

    Converted react-icons/md to icon components

--- src/components/Icon/icons/index.ts
+++ src/components/Icon/icons/index.ts
@@ -1,7 +1,19 @@
 import IconArrowLeft from "./IconArrowLeft";
 import IconArrowRight from "./IconArrowRight";
+import IconChevronDown from "./IconChevronDown";
 import IconDarkModeSwitch from "./IconDarkModeSwitch";
 import IconDeny from "./IconDeny";
+import IconLink from "./IconLink";
+import IconPending from "./IconPending";
 import IconX from "./IconX";
 
-export { IconArrowRight, IconArrowLeft, IconDarkModeSwitch, IconDeny, IconX };
+export {
+  IconArrowRight,
+  IconArrowLeft,
+  IconChevronDown,
+  IconDarkModeSwitch,
+  IconDeny,
+  IconLink,
+  IconPending,
+  IconX,
+};
