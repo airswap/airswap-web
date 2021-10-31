b2a03da826c3cf0823099c437e98815a98f86510
Sven Anders Robbestad <robbestad@gmail.com>
2021-10-31 17:43:57 +0100

    add todo

--- src/components/Toasts/ToastController.tsx
+++ src/components/Toasts/ToastController.tsx
@@ -32,6 +32,12 @@ export const notifyTransaction = (
     chainId
   ) {
     const tx: SubmittedOrder = transaction as SubmittedOrder;
+    /* todo fix toaster for multiple tabs or apps
+        now that we have a listener, you can have multiple
+        tabs open that receives the same order event. Only one redux
+        store will have the order, and the others won't. That will
+        throw an error here if we don't check for `order` inside `tx`
+     */
     if (tx?.order) {
       const senderToken = findEthOrTokenByAddress(
         tx.order.senderToken,
