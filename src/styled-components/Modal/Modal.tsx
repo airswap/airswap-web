e281fac00a310f89904203694db79a66542b0e8c
gpxl-dev <greypixel@gpxl.tech>
2021-11-05 02:45:12 +0000

    fix join modal size and position (#313)

--- src/styled-components/Modal/Modal.tsx
+++ src/styled-components/Modal/Modal.tsx
@@ -4,16 +4,28 @@ import Modal from "styled-react-modal";
 
 import IconButton from "../../components/IconButton/IconButton";
 import { InfoSubHeading, Title } from "../../components/Typography/Typography";
+import breakPoints from "../../style/breakpoints";
+import { sizes } from "../../style/sizes";
 
 export const StyledModal = Modal.styled`
   position: relative;
+  top: 1rem;
   border: 1px solid ${(props: { theme: DefaultTheme }) =>
     props.theme.colors.borderGrey};
   border-radius: 0.1875rem;
   width: 37.5rem;
+  min-height: 30rem;
   padding: 2.5rem 2.125rem;
   background: ${(props: { theme: DefaultTheme }) =>
     props.theme.colors.darkGrey};
+
+  @media ${breakPoints.tabletPortraitUp} {
+    left: calc(${sizes.toolBarWidth}/2);
+  }
+
+  @media ${breakPoints.tabletLandscapeUp} {
+    left: 0;
+  }
 `;
 
 export const ModalCloseButton = styled(IconButton)`
