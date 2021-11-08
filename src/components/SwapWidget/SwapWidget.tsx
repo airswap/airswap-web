c45cc3ab3c9d9b54cc2f69cfa7efd3c751987905
Piers <bonkbonksourman@gmail.com>
2021-11-08 03:56:43 +0100

    264: Removed test code

--- src/components/SwapWidget/SwapWidget.tsx
+++ src/components/SwapWidget/SwapWidget.tsx
@@ -646,7 +646,7 @@ const SwapWidget: FC<SwapWidgetPropsType> = ({
             onMaxButtonClick={() => setBaseAmount(maxAmount || "0")}
             side="sell"
             tradeNotAllowed={pairUnavailable}
-            isRequesting={isRequestingQuotes || isWrapping || isWrapping}
+            isRequesting={isRequestingQuotes}
             // Note that using the quoteAmount from tradeTerms will stop this
             // updating when the user clicks the take button.
             quoteAmount={
