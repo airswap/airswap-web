b540d4a9991961fa0dd3a0d5d59a2204160499b6
Sven Anders Robbestad <robbestad@gmail.com>
2021-11-23 09:59:43 +0100

    remove defaultns

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -17,7 +17,6 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: "en",
-    defaultNS: "toast",
     load: "languageOnly",
     debug: process.env.NODE_ENV !== "production",
     interpolation: {
