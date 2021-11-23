19f0b5675fef3aba5578c35f61874df9cbe58435
Sven Anders Robbestad <robbestad@gmail.com>
2021-11-23 09:19:26 +0100

    add french

--- src/constants/locales.ts
+++ src/constants/locales.ts
@@ -1,6 +1,7 @@
 export const SUPPORTED_LOCALES = [
   // order as they appear in the language dropdown
   "en",
+  "fr",
   "nb",
 ];
 
@@ -11,4 +12,5 @@ export const DEFAULT_LOCALE: SupportedLocale = "en";
 export const LOCALE_LABEL: { [locale in SupportedLocale]: string } = {
   en: "English",
   nb: "Norsk Bokmål",
+  fr: "Français",
 };
