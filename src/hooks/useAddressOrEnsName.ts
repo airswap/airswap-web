65310747b6fcc27d871e5bb1d3f34d1af27bf21a
Denimsun Basumatary <denimsunbasumatary@outlook.com>
2021-12-17 01:03:35 +0530

    Show domain name on wallet button

--- src/hooks/useAddressOrEnsName.ts
+++ src/hooks/useAddressOrEnsName.ts
@@ -1,20 +1,30 @@
 import { useState, useLayoutEffect } from "react";
 
 import { Web3Provider } from "@ethersproject/providers";
+import { UAuthConnector } from "@uauth/web3-react";
 import { useWeb3React } from "@web3-react/core";
 
 import truncateEthAddress from "truncate-eth-address";
+
+import { AbstractConnector } from "../constants/supportedWalletProviders";
 
 // This is an in-memory cache that will be lost when we refresh the page, as
 // ENS records may change, but we probably only need to check once between
 // refreshes. Format: { [chainId]: { [address]: name | null }}
 const ensCachedResponses: Record<number, Record<string, string | null>> = {};
 
-const useAddressOrEnsName = (
+const getUDomain = async (connector: AbstractConnector | undefined) => {
+  if (connector instanceof UAuthConnector) {
+    const { sub } = await (connector as UAuthConnector).uauth.user();
+    return sub;
+  }
+};
+
+const useAddressEnsOrUDomain = (
   address: string | null,
   truncate: boolean = true
 ) => {
-  const { library, chainId } = useWeb3React<Web3Provider>();
+  const { library, chainId, connector } = useWeb3React<Web3Provider>();
 
   const fallback = truncate
     ? address
@@ -32,6 +42,10 @@ const useAddressOrEnsName = (
     const cached = ensCachedResponses[chainId]?.[address];
     if (cached !== undefined) {
       setResult(cached || fallback);
+    } else if (connector instanceof UAuthConnector) {
+      getUDomain(connector).then((domain) => {
+        setResult(domain || fallback);
+      });
     } else {
       library
         .lookupAddress(address)
@@ -46,9 +60,11 @@ const useAddressOrEnsName = (
           setResult(fallback);
         });
     }
-  }, [library, address, chainId, fallback]);
+
+    // Get domain if connector is Unstoppable Domain
+  }, [library, address, chainId, fallback, connector]);
 
   return result;
 };
 
-export default useAddressOrEnsName;
+export default useAddressEnsOrUDomain;
