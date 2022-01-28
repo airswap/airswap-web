93fdf9dc18edba2fd52a7ccab8cfade64fb9a3d2
ben <phas0ruk@gmail.com>
2022-01-27 17:02:03 -0800

    adding /whitepaper route and its static HTML and other page assets

---
+++ src/components/Whitepaper/Whitepaper.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+
+import "./Whitepaper.css";
+import htmlContent from "./WhitepaperContent";
+
+const Whitepaper: FC = () => {
+  return (
+    <>
+      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+    </>
+  );
+};
+
+export default Whitepaper;
