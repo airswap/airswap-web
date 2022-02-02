6d78c10e4391b0be200699b9dd678cc09718fc86
ben <phas0ruk@gmail.com>
2022-02-02 14:45:44 -0800

    runs prettier on Whitepaper.tsx

--- src/components/Whitepaper/Whitepaper.tsx
+++ src/components/Whitepaper/Whitepaper.tsx
@@ -3,9 +3,7 @@ import React, { FC } from "react";
 import htmlContent from "./whitepaperContent";
 
 const Whitepaper: FC = () => {
-  return (
-      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-  );
+  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
 };
 
 export default Whitepaper;
