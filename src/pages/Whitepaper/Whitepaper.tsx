29d7715bd3229a64263ea6c08fc839b1bc160555
ben <phas0ruk@gmail.com>
2022-02-11 09:41:48 -0800

    removes body and head tag from whitepaperContent.tsx, this is injected by React
    (cherry picked from commit 36d5a97d59787f9a88c8d6e75aefada4fac0f098)

--- src/pages/Whitepaper/Whitepaper.tsx
+++ src/pages/Whitepaper/Whitepaper.tsx
@@ -1,14 +1,16 @@
 import React, { FC } from "react";
 
-import htmlContent from "./whitepaperContent";
 import HelmetContainer from "../../components/HelmetContainer/HelmetContainer";
+import htmlContent from "./whitepaperContent";
 
 const Whitepaper: FC = () => {
   return (
     <>
       <HelmetContainer
         title={"AirSwap Whitepaper"}
-        description={"Learn how the Airswap protocol can save you money by powering gasless p2p crypto trades."}
+        description={
+          "Learn how the Airswap protocol can save you money by powering gasless p2p crypto trades."
+        }
       />
 
       <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
