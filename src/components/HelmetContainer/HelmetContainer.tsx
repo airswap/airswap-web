3fd33851ca558e29441848e898ef8f6de6ed7725
Piers <bonkbonksourman@gmail.com>
2022-02-21 15:00:59 +0100

    185: Fixed Brigand merge request comment

--- src/components/HelmetContainer/HelmetContainer.tsx
+++ src/components/HelmetContainer/HelmetContainer.tsx
@@ -8,17 +8,17 @@ type HelmetContainerProps = {
 
 const HelmetContainer: FC<HelmetContainerProps> = ({
   title,
-  description,
+  description = "",
   children,
 }) => {
   return (
     <Helmet>
       <title>{title}</title>
-      {description && <meta name="description" content={description} />}
+      <meta name="description" content={description} />
       <meta property="og:title" content={title} />
-      {description && <meta property="og:description" content={description} />}
+      <meta property="og:description" content={description} />
       <meta name="twitter:title" content={title} />
-      {description && <meta name="twitter:description" content={description} />}
+      <meta name="twitter:description" content={description} />
       {children}
     </Helmet>
   );
