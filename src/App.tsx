003a66ac476525d19a00bb5e2b75f2598b7d46e9
Sven Anders Robbestad <1560743+robbestad@users.noreply.github.com>
2022-02-24 10:50:10 +0100

    Move whitepaper to public folder (#488) (#489)
    Serve whitepaper with the established whitepaper.htm URL

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import BigNumber from "bignumber.js";
 import HelmetContainer from "./components/HelmetContainer/HelmetContainer";
 import "./i18n/i18n";
 import Home from "./pages/Home/Home";
-import Whitepaper from "./pages/Whitepaper/Whitepaper";
-import { AppRoutes } from "./routes";
 
 // 1e+9 is the highest possible number
 BigNumber.config({ EXPONENTIAL_AT: 1e9 });
@@ -23,10 +21,6 @@ const App = (): JSX.Element => {
 
       <Router>
         <Switch>
-          <Route exact path={`/${AppRoutes.whitepaper}`}>
-            <Whitepaper />
-          </Route>
-
           <Route>
             <Home />
           </Route>
