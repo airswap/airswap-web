3cc2fa31ff2c3455f5d43a6dafa139a1e257bb07
Piers <bonkbonksourman@gmail.com>
2022-07-10 20:37:13 +0200

    576: Added getExpiryTranslation helper

--- src/components/ExpiryIndicator/ExpiryIndicator.styles.tsx
+++ src/components/ExpiryIndicator/ExpiryIndicator.styles.tsx
@@ -1,7 +1,5 @@
 import styled from "styled-components/macro";
 
-import { Pill } from "../../styled-components/Pill/Pill";
-
 export const Strong = styled.strong`
   margin-left: 0.375rem;
   color: ${({ theme }) => theme.colors.white};
