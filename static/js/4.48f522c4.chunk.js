e97438c9753e8ed1ba47ac743850e1f670cdfc87
dmosites <mosites@gmail.com>
2022-08-03 14:40:19 +0000

    Deploying to gh-pages from @ airswap/airswap-web@7d4f69ea41a77382aba3d9d8687ac96b7bf09f52 🚀

---
+++ static/js/4.48f522c4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpairswap-web"]=this["webpackJsonpairswap-web"]||[]).push([[4],{705:function(p,s){}}]);
+//# sourceMappingURL=4.48f522c4.chunk.js.map
--- static/js/4.a4ca2171.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpairswap-web"]=this["webpackJsonpairswap-web"]||[]).push([[4],{677:function(p,s){}}]);
-//# sourceMappingURL=4.a4ca2171.chunk.js.map
