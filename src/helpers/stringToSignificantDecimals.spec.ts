2e0888ca8cc814ae0588d112088e444d50c9babc
Coltrane Yan <coltraneyan@gmail.com>
2022-08-10 21:36:30 -0400

    stringtodecimal helper overwrite and test update

--- src/helpers/stringToSignificantDecimals.spec.ts
+++ src/helpers/stringToSignificantDecimals.spec.ts
@@ -13,11 +13,18 @@ describe("stringToSignificantDecimals", () => {
     expect(stringToSignificantDecimals("1234.56789", 4)).toBe("1234.5678");
   });
 
+  it("Should (only) ignore insignificant decimal places", () => {
+    expect(stringToSignificantDecimals("0.0005607891", 4)).toBe("0.0005607");
+  });
+
   it("Should only ignore decimal leading zeroes if number is less than 0", () => {
-    expect(stringToSignificantDecimals("1234.000056789", 4)).toBe("1234.0000");
+    expect(stringToSignificantDecimals("1234.000056789", 5)).toBe("1234.00005");
+    expect(stringToSignificantDecimals("1234.000056789", 4)).toBe("1234");
   });
 
   it("Should (only) ignore insignificant decimal places", () => {
-    expect(stringToSignificantDecimals("0.0005607891", 4)).toBe("0.0005607");
+    expect(stringToSignificantDecimals("100000000000.0000000000001", 10)).toBe(
+      "> 999999999.9"
+    );
   });
 });
