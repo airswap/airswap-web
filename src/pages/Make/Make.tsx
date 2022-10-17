1e2e16eb8a26faa3e67cc684c0dbd4c8bde91469
piersss <bonkbonksourman@gmail.com>
2022-10-17 11:41:43 +0200

    Fixed bug where orderState doesn't get cleared after making new order.

--- src/pages/Make/Make.tsx
+++ src/pages/Make/Make.tsx
@@ -1,9 +1,17 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 
+import { useAppDispatch } from "../../app/hooks";
 import MakeWidget from "../../components/MakeWidget/MakeWidget";
+import { reset } from "../../features/takeOtc/takeOtcSlice";
 import { StyledPage } from "./Make.styles";
 
 const MakePage: FC = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
+
   return (
     <StyledPage>
       <MakeWidget />
