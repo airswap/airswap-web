59a2fe315fb875be910e7248e10673f10316d05e
Don Mosites <mosites@gmail.com>
2022-10-24 21:09:28 +0200

    Add Procfile, index.js, express; update craco

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,7 @@
 // Allows configuration of create-react-app build process.
 // ref: https://github.com/gsoft-inc/craco/blob/master/packages/craco/README.md
 
+const fs = require('fs')
 const cracoEnvPlugin = require('craco-plugin-env')
 
 module.exports = {
@@ -14,12 +15,12 @@ module.exports = {
       plugin: cracoEnvPlugin,
       options: {
         variables: {
-          BUILD_VERSION: require('child_process')
+          BUILD_VERSION: fs.existsSync('.git') ? require('child_process')
               .execSync('git rev-parse HEAD', { cwd: __dirname })
-              .toString().trim(),
-          BUILD_DATE: require('child_process')
+              .toString().trim() : 'DEV',
+          BUILD_DATE: fs.existsSync('.git') ? require('child_process')
               .execSync('git show -s --format=%ci', { cwd: __dirname })
-              .toString().trim(),
+              .toString().trim() : new Date().toLocaleDateString(),
         }
       }
     }
