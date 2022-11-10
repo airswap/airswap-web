2454f22405d16adf5969c1871f5cd710e3eef4fb
ilam0602 <isaac.lam@live.com>
2022-11-09 16:48:46 -0800

    650: remove unnecessary imports

--- src/helpers/getHumanReadableNumber.spec.ts
+++ src/helpers/getHumanReadableNumber.spec.ts
@@ -1,50 +1,49 @@
-
 import { getHumanReadableNumber } from "./getHumanReadableNumber";
 
-describe("getHumanReadableNumber" , () => {
-    it("10000 should be 10k", () =>{
-        expect(getHumanReadableNumber("10000")).toBe("10k");
-    });
-    it("10000.000001 should be 10k", () =>{
-        expect(getHumanReadableNumber("10000.0001")).toBe("10k");
-    });
-    it("12345 should be 12.3k", () =>{
-        expect(getHumanReadableNumber("12345")).toBe("12.3k");
-    });
-    it("1000000 should be 1M", () =>{
-        expect(getHumanReadableNumber("1000000")).toBe("1M");
-    });
-    it("9876543 should be 9.8M", () =>{
-        expect(getHumanReadableNumber("9876543")).toBe("9.8M");
-    });
-    it("0.001234 should be cut off to .0012", () =>{
-        expect(getHumanReadableNumber("0.001234")).toBe("0.0012");
-    });
-    it("0.0000987654 should be cut off to 0.000098", () =>{
-        expect(getHumanReadableNumber("0.0000987654")).toBe("0.000098");
-    });
-    it("10000000 should be 10M", () =>{
-        expect(getHumanReadableNumber("10000000")).toBe("10M");
-    });
-    it("12340000 should be 12.3M", () =>{
-        expect(getHumanReadableNumber("12340000")).toBe("12.3M");
-    });
-    it("123450000 should be 123.4M", () =>{
-        expect(getHumanReadableNumber("123450000")).toBe("123.4M");
-    });
-    it("1234560000 should be 1234.5M", () =>{
-        expect(getHumanReadableNumber("1234560000")).toBe("1234.5M");
-    });
-    it("0.00012 should be unaffected", () =>{
-        expect(getHumanReadableNumber("0.00012")).toBe("0.00012");
-    });
-    it("0.12 should be unaffected", () =>{
-        expect(getHumanReadableNumber("0.12")).toBe("0.12");
-    });
-    it("123.4 should be unaffected", () =>{
-        expect(getHumanReadableNumber("123.4")).toBe("123.4");
-    });
-    it("1234.5 should be 1.2k", () =>{
-        expect(getHumanReadableNumber("1234.5")).toBe("1.2k");
-    });
+describe("getHumanReadableNumber", () => {
+  it("10000 should be 10k", () => {
+    expect(getHumanReadableNumber("10000")).toBe("10k");
+  });
+  it("10000.000001 should be 10k", () => {
+    expect(getHumanReadableNumber("10000.0001")).toBe("10k");
+  });
+  it("12345 should be 12.3k", () => {
+    expect(getHumanReadableNumber("12345")).toBe("12.3k");
+  });
+  it("1000000 should be 1M", () => {
+    expect(getHumanReadableNumber("1000000")).toBe("1M");
+  });
+  it("9876543 should be 9.8M", () => {
+    expect(getHumanReadableNumber("9876543")).toBe("9.8M");
+  });
+  it("0.001234 should be cut off to .0012", () => {
+    expect(getHumanReadableNumber("0.001234")).toBe("0.0012");
+  });
+  it("0.0000987654 should be cut off to 0.000098", () => {
+    expect(getHumanReadableNumber("0.0000987654")).toBe("0.000098");
+  });
+  it("10000000 should be 10M", () => {
+    expect(getHumanReadableNumber("10000000")).toBe("10M");
+  });
+  it("12340000 should be 12.3M", () => {
+    expect(getHumanReadableNumber("12340000")).toBe("12.3M");
+  });
+  it("123450000 should be 123.4M", () => {
+    expect(getHumanReadableNumber("123450000")).toBe("123.4M");
+  });
+  it("1234560000 should be 1234.5M", () => {
+    expect(getHumanReadableNumber("1234560000")).toBe("1234.5M");
+  });
+  it("0.00012 should be unaffected", () => {
+    expect(getHumanReadableNumber("0.00012")).toBe("0.00012");
+  });
+  it("0.12 should be unaffected", () => {
+    expect(getHumanReadableNumber("0.12")).toBe("0.12");
+  });
+  it("123.4 should be unaffected", () => {
+    expect(getHumanReadableNumber("123.4")).toBe("123.4");
+  });
+  it("1234.5 should be 1.2k", () => {
+    expect(getHumanReadableNumber("1234.5")).toBe("1.2k");
+  });
 });
--- src/helpers/getHumanReadableNumber.ts
+++ src/helpers/getHumanReadableNumber.ts
@@ -1,63 +1,78 @@
-import { el } from "date-fns/locale";
-import { before } from "lodash";
+export const getHumanReadableNumber: (input: string) => string = (input) => {
+  const [beforeDecimalPoint, afterDecimalPoint] = input.split(".");
+  //sets to 2 sig digits after decimal if nothing before decimal
+  //and (numSigDigits -1) = 1 sig digit after decimal
+  //if there is sig digit in front of decimal
+  const numSigDigits = 2;
+  let readableNumber = "";
 
-export const getHumanReadableNumber: (
-    input:string
-) => string = (input)  =>{
-
-    const [beforeDecimalPoint, afterDecimalPoint] = input.split(".");
-    //sets to 2 sig digits after decimal if nothing before decimal
-    //and (numSigDigits -1) = 1 sig digit after decimal 
-    //if there is sig digit in front of decimal
-    const numSigDigits = 2;
-    let readableNumber = "";
-
-    //check if there is anything before decimal point
-    if(beforeDecimalPoint.length < 4 || beforeDecimalPoint == "0" ){
-        //if there is nothing before decimal point check if anything after decimal point 
-        //needs to be rounded 2 sig digits
-        let sigDigit = afterDecimalPoint[0];
-        let iter  = 0;
-        //iterate until first significant digit
-        while(sigDigit == '0'){
-            iter += 1;
-            sigDigit = afterDecimalPoint[iter];
-        }
-        //do nothing if no trimming is needed
-        if (afterDecimalPoint.length - (iter +1) < numSigDigits ){
-            return input;
-        }
-        //else have to trim
-        else{
-            readableNumber = beforeDecimalPoint + "." + afterDecimalPoint.substring(0,iter+numSigDigits);
-            return readableNumber;
-        }
+  //check if there is anything before decimal point
+  if (beforeDecimalPoint.length < 4 || beforeDecimalPoint == "0") {
+    //if there is nothing before decimal point check if anything after decimal point
+    //needs to be rounded 2 sig digits
+    let sigDigit = afterDecimalPoint[0];
+    let iter = 0;
+    //iterate until first significant digit
+    while (sigDigit == "0") {
+      iter += 1;
+      sigDigit = afterDecimalPoint[iter];
     }
-    else{
-        //first check to see if M is needed
-        if(beforeDecimalPoint.length >= 7){
-            readableNumber = beforeDecimalPoint.substring(0,beforeDecimalPoint.length - (7 - (numSigDigits-1)));
-            //check if after decimal point is significant
-            if(beforeDecimalPoint[beforeDecimalPoint.length - (7 - (numSigDigits-1))] !== '0'){
-                readableNumber += ".";
-                readableNumber += beforeDecimalPoint[beforeDecimalPoint.length - (7 - (numSigDigits-1))];
-            }
-            readableNumber += "M";
-            return readableNumber;
-        }
-        //now check if k is needed
-        if(beforeDecimalPoint.length >= 4){
-            readableNumber = beforeDecimalPoint.substring(0,beforeDecimalPoint.length - (4 - (numSigDigits-1)));
-            //check if after decimal point is significant
-            if(beforeDecimalPoint[beforeDecimalPoint.length - (4 - (numSigDigits-1))] !== '0'){
-                readableNumber += ".";
-                readableNumber += beforeDecimalPoint[beforeDecimalPoint.length - (4 - (numSigDigits-1))];
-            }
-            readableNumber += "k";
-            return readableNumber;
-        }
- 
+    //do nothing if no trimming is needed
+    if (afterDecimalPoint.length - (iter + 1) < numSigDigits) {
+      return input;
     }
+    //else have to trim
+    else {
+      readableNumber =
+        beforeDecimalPoint +
+        "." +
+        afterDecimalPoint.substring(0, iter + numSigDigits);
+      return readableNumber;
+    }
+  } else {
+    //first check to see if M is needed
+    if (beforeDecimalPoint.length >= 7) {
+      readableNumber = beforeDecimalPoint.substring(
+        0,
+        beforeDecimalPoint.length - (7 - (numSigDigits - 1))
+      );
+      //check if after decimal point is significant
+      if (
+        beforeDecimalPoint[
+          beforeDecimalPoint.length - (7 - (numSigDigits - 1))
+        ] !== "0"
+      ) {
+        readableNumber += ".";
+        readableNumber +=
+          beforeDecimalPoint[
+            beforeDecimalPoint.length - (7 - (numSigDigits - 1))
+          ];
+      }
+      readableNumber += "M";
+      return readableNumber;
+    }
+    //now check if k is needed
+    if (beforeDecimalPoint.length >= 4) {
+      readableNumber = beforeDecimalPoint.substring(
+        0,
+        beforeDecimalPoint.length - (4 - (numSigDigits - 1))
+      );
+      //check if after decimal point is significant
+      if (
+        beforeDecimalPoint[
+          beforeDecimalPoint.length - (4 - (numSigDigits - 1))
+        ] !== "0"
+      ) {
+        readableNumber += ".";
+        readableNumber +=
+          beforeDecimalPoint[
+            beforeDecimalPoint.length - (4 - (numSigDigits - 1))
+          ];
+      }
+      readableNumber += "k";
+      return readableNumber;
+    }
+  }
 
-    return input;
-}
+  return input;
+};
