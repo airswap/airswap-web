372ccf7305970c187ab7efcc6c476b7ee493ab92
ilam0602 <isaac.lam@live.com>
2022-11-10 02:09:23 -0800

    650: misc changes

--- src/helpers/getHumanReadableNumber.ts
+++ src/helpers/getHumanReadableNumber.ts
@@ -8,7 +8,7 @@ export const getHumanReadableNumber: (input: string) => string = (input) => {
   let readableNumber = "";
   let suffixTracker = 0;
 
-  //check if anything before decimal needs to be added 
+  //check if anything before decimal needs to be added
   if (Number(beforeDecimalPoint) === 0) {
     //if there is nothing before decimal point check if anything after decimal point
     //needs to be rounded 2 sig digits
@@ -31,72 +31,69 @@ export const getHumanReadableNumber: (input: string) => string = (input) => {
         afterDecimalPoint.substring(0, iter + numSigDigits);
       return readableNumber;
     }
-  //there are digits before the decimal
-  }else {
-      if(beforeDecimalPoint.length>=10){
-        suffixTracker = 10;
-      }
-      else if(beforeDecimalPoint.length>=7){
-        suffixTracker = 7;
-      }else if(beforeDecimalPoint.length>=4){
-        suffixTracker = 4
-      }
-      //shorten substring if suffix is needed
-      if(suffixTracker !== 0){
-        readableNumber = beforeDecimalPoint.substring(
-          0,
-          beforeDecimalPoint.length - (suffixTracker - (numSigDigits - 1))
-          );
-          if (
-            beforeDecimalPoint[
-              beforeDecimalPoint.length - (suffixTracker - (numSigDigits - 1))
-            ] !== "0"
-          ) {
-            readableNumber += ".";
-            readableNumber +=
-              beforeDecimalPoint[
-                beforeDecimalPoint.length - (suffixTracker - (numSigDigits - 1))
-              ];
-            readableNumber +=
-              beforeDecimalPoint[
-                beforeDecimalPoint.length - (suffixTracker - (numSigDigits))
-              ];
-          }
-
-      }
-      else{
-        readableNumber = input;
-      }
-      let numExtraZeros = 0;
-      let decIndex = readableNumber.indexOf(".");
-      //get to 4 sig digits
-      if(readableNumber.length> maxDigits && (decIndex !== -1)&&(decIndex < (maxDigits))){
-        readableNumber = readableNumber.substring(0,maxDigits+1);
-      }
-      else if(readableNumber.length> maxDigits){
-        if(decIndex!==-1){
-          numExtraZeros = decIndex - maxDigits;
-        }
-        else{
-          numExtraZeros = readableNumber.length - maxDigits;
-        }
-        readableNumber = readableNumber.substring(0,maxDigits);
-      }
-      //add extra zeroes if needed
-      while(numExtraZeros>0){
-        readableNumber+="0";
-        numExtraZeros-=1;
-      }
-      //add respective suffix
-      if(suffixTracker === 10){
-        readableNumber += "B";
-      }
-      else if(suffixTracker === 7){
-        readableNumber += "M";
-      }
-      else if(suffixTracker === 4){
-        readableNumber += "k";
-      }
+    //there are digits before the decimal
+  } else {
+    if (beforeDecimalPoint.length >= 10) {
+      suffixTracker = 10;
+    } else if (beforeDecimalPoint.length >= 7) {
+      suffixTracker = 7;
+    } else if (beforeDecimalPoint.length >= 4) {
+      suffixTracker = 4;
     }
-    return readableNumber;
-    };
+    //shorten substring if suffix is needed
+    if (suffixTracker !== 0) {
+      readableNumber = beforeDecimalPoint.substring(
+        0,
+        beforeDecimalPoint.length - (suffixTracker - (numSigDigits - 1))
+      );
+      if (
+        beforeDecimalPoint[
+          beforeDecimalPoint.length - (suffixTracker - (numSigDigits - 1))
+        ] !== "0"
+      ) {
+        readableNumber += ".";
+        readableNumber +=
+          beforeDecimalPoint[
+            beforeDecimalPoint.length - (suffixTracker - (numSigDigits - 1))
+          ];
+        readableNumber +=
+          beforeDecimalPoint[
+            beforeDecimalPoint.length - (suffixTracker - numSigDigits)
+          ];
+      }
+    } else {
+      readableNumber = input;
+    }
+    let numExtraZeros = 0;
+    let decIndex = readableNumber.indexOf(".");
+    //get to 4 sig digits
+    if (
+      readableNumber.length > maxDigits &&
+      decIndex !== -1 &&
+      decIndex < maxDigits
+    ) {
+      readableNumber = readableNumber.substring(0, maxDigits + 1);
+    } else if (readableNumber.length > maxDigits) {
+      if (decIndex !== -1) {
+        numExtraZeros = decIndex - maxDigits;
+      } else {
+        numExtraZeros = readableNumber.length - maxDigits;
+      }
+      readableNumber = readableNumber.substring(0, maxDigits);
+    }
+    //add extra zeroes if needed
+    while (numExtraZeros > 0) {
+      readableNumber += "0";
+      numExtraZeros -= 1;
+    }
+    //add respective suffix
+    if (suffixTracker === 10) {
+      readableNumber += "B";
+    } else if (suffixTracker === 7) {
+      readableNumber += "M";
+    } else if (suffixTracker === 4) {
+      readableNumber += "k";
+    }
+  }
+  return readableNumber;
+};
