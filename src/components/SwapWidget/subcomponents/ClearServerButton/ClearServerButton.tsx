bda04a0f190b21c8b2694a2ec15332a5f9092762
Mike Starr <imhs@protonmail.com>
2023-06-07 10:51:41 -0400

    fixed ClearServerButton to update url

--- src/components/SwapWidget/subcomponents/ClearServerButton/ClearServerButton.tsx
+++ src/components/SwapWidget/subcomponents/ClearServerButton/ClearServerButton.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 import { useAppDispatch, useAppSelector } from "../../../../app/hooks";
 import {
@@ -10,12 +11,26 @@ import {
   ClearServerButtonText,
 } from "./ClearServerButton.styles";
 
+/**
+ * @remarks - when clicked, this button sets serverUrl in Redux to null, then changes search history
+ * @returns
+ */
 const ClearServerButton = () => {
   const serverUrl = useAppSelector(selectServerUrl);
   const dispatch = useAppDispatch();
 
+  const history = useHistory();
+
   const handleClearServerUrl = () => {
     dispatch(setServerUrl(null));
+
+    let location = history.location;
+    const searchParams = new URLSearchParams(location.search);
+    searchParams.delete("serverUrl");
+    history.push({
+      ...location,
+      search: searchParams.toString(),
+    });
   };
 
   return (
