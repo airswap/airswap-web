6e9157aaf2484c4510441d60a43961cfa6bc1d22
Don Mosites <mosites@gmail.com>
2023-12-08 18:39:40 +0100

    restrict to linea

--- src/helpers/switchToDefaultChain.ts
+++ src/helpers/switchToDefaultChain.ts
@@ -4,7 +4,7 @@ import switchToChain from "./switchToChain";
 
 const switchToDefaultChain = () => {
   try {
-    switchToChain(ChainIds.MAINNET);
+    switchToChain(ChainIds.LINEA);
   } catch (e) {
     // unable to switch network, but doesn't matter too much as button
     // looks like a call to action in this case anyway.
