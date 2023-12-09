4816a00b777a36f17ce211d262222a543ab5ac2c
Don Mosites <mosites@gmail.com>
2023-12-09 11:27:36 +0100

    prettier

--- src/constants/rpcUrls.ts
+++ src/constants/rpcUrls.ts
@@ -1,3 +1,8 @@
 import { apiUrls, mainnets, testnets } from "@airswap/constants";
-const rpcUrls: Record<number, string> = mainnets.concat(testnets).map((chainId) => process.env[`REACT_APP_RPC_URL_${chainId}`] || apiUrls[chainId]);
-export default rpcUrls
+
+const rpcUrls: Record<number, string> = mainnets
+  .concat(testnets)
+  .map(
+    (chainId) => process.env[`REACT_APP_RPC_URL_${chainId}`] || apiUrls[chainId]
+  );
+export default rpcUrls;
