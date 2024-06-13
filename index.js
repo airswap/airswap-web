31cfeaafb156203f0461b28e37ad9b1c221478a1
piersss <bonkbonksourman@gmail.com>
2024-06-13 21:22:17 +0200

    908: Added cors to production server

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+  origin: 'https://app.safe.global',
+  methods: 'GET',
+  allowedHeaders: 'X-Requested-With, content-type, Authorization',
+  credentials: true
+}));
+
 app.use(express.static('./build'));
+
 app.listen(process.env.PORT || 3000);
