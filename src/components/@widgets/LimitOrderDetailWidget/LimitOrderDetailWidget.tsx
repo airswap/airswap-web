a392c4963a18aa062bfc56b34da8b6c81183b69d
makkelie_dev <makkelie@protonmail.com>
2025-05-04 18:09:00 +0200

    fixed action buttons of filled limit order

--- src/components/@widgets/LimitOrderDetailWidget/LimitOrderDetailWidget.tsx
+++ src/components/@widgets/LimitOrderDetailWidget/LimitOrderDetailWidget.tsx
@@ -310,14 +310,8 @@ const LimitOrderDetailWidget: FC<LimitOrderDetailWidgetProps> = ({
       return;
     }
 
-    // TODO: Support AppTokenInfo
     dispatch(
-      approve(
-        customSignerAmountPlusFee,
-        signerToken as TokenInfo,
-        library,
-        "SwapERC20"
-      )
+      approve(customSignerAmountPlusFee, signerToken, library, "SwapERC20")
     );
   };
 
@@ -538,7 +532,7 @@ const LimitOrderDetailWidget: FC<LimitOrderDetailWidgetProps> = ({
           isExpired={orderStatus === OrderStatus.expired}
           isCanceled={orderStatus === OrderStatus.canceled}
           isLimitOrder={true}
-          isTaken={orderStatus === OrderStatus.taken}
+          isTaken={orderStatus === OrderStatus.filled}
           isDifferentChainId={walletChainIdIsDifferentThanOrderChainId}
           isIntendedRecipient={true}
           isLoading={isBalanceLoading}
